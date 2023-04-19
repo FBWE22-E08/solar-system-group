d62f4ef72b9bfbaffc4e23efd62c94a03896d032
shinheeberlin <shinhee.chae@digitalcareerinstitute.org>
2023-04-19 12:48:07 +0200

    source code updated

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//* SERVER FOR SOLAR SYSTEM
+import express from 'express'
+import cors from 'cors';
+
+const app = express();
+const port = 5000
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+    res.status(StatusCodes.OK).send(`<h1>Welcome to our port ${port} 🐱</h1>`)
+})
+
+app.listen(port, () => {
+    console.log(`app is up and running @ http://localhost:${port}`) 
+ });
+ 
